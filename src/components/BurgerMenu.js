abd76c30d2f23e96c7a527b10532ec92c72066e6
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-18 16:56:32 -0600

    Add burger menu button component

---
+++ src/components/BurgerMenu.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+export const BurgerMenu = () => {
+  const [focus, setFocus] = useState(false)
+
+  return (
+    <div className='burger-menu'
+         onClick={() => setFocus(!focus)}>
+      <div className={`line one ${focus ? 'focus' : ' '}`} />
+      <div className={`line two ${focus ? 'focus' : ' '}`} />
+      <div className={`line three ${focus ? 'focus' : ' '}`}/>
+    </div>
+  )
+}
