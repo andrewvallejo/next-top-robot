16c65610298599ea535f85ee78d73f7032493dae
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-18 13:50:14 -0600

    Add all JSX needed to copy wire frame

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,21 @@ import logo from  '../assets/mr-logo.png';
 export const NavBar = () => {
   return (
     <header className='main-header'>
-      <img alt='A logo of Mondo Robot' src={logo} className='logo'/>
+      <section className='left-container'>
+        <NavLink to='/robots'>
+          <img alt='A logo of Mondo Robot' src={logo} className='logo'/>
+        </NavLink>
+        <nav className='nav-container'>
+          <NavLink to='/robots'>Robots</NavLink>
+          <NavLink to='/results'>Results</NavLink>
+        </nav>
+      </section>
+      <section className='right-container'>
+        <nav className='nav-container'>
+          <NavLink to='/admin'>admin</NavLink>
+          <NavLink to='/'>Log Out</NavLink>
+        </nav>
+      </section>
     </header>
   );
 }
