5d6d47e4f62616cf40f48101d99b7d863a9d903c
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-18 17:41:14 -0600

    Add burger menu component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from  '../assets/mr-logo.png';
+import { BurgerMenu } from './BurgerMenu';
 
 export const NavBar = () => {
   return (
@@ -18,6 +19,7 @@ export const NavBar = () => {
         <nav className='nav-container'>
           <NavLink to='/admin'>Admin</NavLink>
           <NavLink to='/'>Log Out</NavLink>
+          <BurgerMenu />
         </nav>
       </section>
     </header>
