cfa8306c147cd239f63c510bbd341a531686ca35
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-18 19:48:19 -0600

    Add component with 4 sample cards for preview

---
+++ src/components/RobotBoard.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Card } from "./Card"
+
+export const RobotBoard = () => {
+  return (
+    <section className='robot-board'>
+       <Card />
+       <Card />
+       <Card />
+       <Card />
+    </section>
+  )
+}
