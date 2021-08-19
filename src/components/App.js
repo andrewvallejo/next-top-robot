22414fda3fa71eed1c2c40af90b87f79362a69b1
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-19 11:56:16 -0600

    Add useState hook for error handling

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Switch } from  'react-router-dom';
 import { BoardHeader } from './BoardHeader';
 import { LoginPortal } from './LoginPortal';
@@ -6,6 +6,7 @@ import { NavBar } from './NavBar';
 import { RobotBoard } from './RobotBoard';
 
 export const App = () => {
+  const [error, setError] = useState('')
 
   return (
     <Switch>
