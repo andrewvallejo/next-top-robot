2db27660a509776726713dbb8eb364bd91f1178b
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-19 11:55:32 -0600

    Refactor the jsx organization in the return

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Route, Switch } from  'react-router-dom';
 import { BoardHeader } from './BoardHeader';
+import { LoginPortal } from './LoginPortal';
 import { NavBar } from './NavBar';
 import { RobotBoard } from './RobotBoard';
 
 export const App = () => {
-  
 
   return (
-    // <Switch>
-    <Route exact path="/robots">
-    <NavBar /> 
-        <main>       
-         <BoardHeader />
-
-            <RobotBoard />
-        </main>
-
+    <Switch>
+      <Route exact path='/'>
+          <LoginPortal />
       </Route>
-    // </Switch>
+      <Route exact path='/robots'>
+        <NavBar /> 
+        <main>       
+          <BoardHeader />
+          <RobotBoard />
+        </main>
+      </Route>
+    </Switch>
   );
 }
 
