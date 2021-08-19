cc112a2f681938dee043e09880f673f8c5113047
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-18 21:28:30 -0600

    Remove btn element for Button component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,14 @@
 import React from "react"
+import { Button } from './Button'
 import bender from '../assets/bender.png'
+
 
 export const Card = () => {
   return (
     <article className='card'>
       <h3>Bender</h3>
       <img className='robot-image' alt='Bender' src={bender} />
-      <button>Vote</button>
+      <Button />
     </article>
   )
 }
