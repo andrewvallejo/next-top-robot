058216912e7966a8f779d0738557758a7a46b64d
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-19 12:07:24 -0600

    Add destructured props and add handler

--- src/components/LoginPortal.js
+++ src/components/LoginPortal.js
@@ -2,15 +2,20 @@ import React, { useState } from 'react'
 import { Button } from './Button'
 import logo from '../assets/mr-logo.png'
 
-export const LoginPortal = () => {
+export const LoginPortal = ({login, error}) => {
   const [credentials, setCredentials] = useState({name: '', email: '', password: ''})
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    login(credentials)
+  }
 
   return (
     <section className='login-board'>
       <article className='portal'>
       <img alt='A logo of Mondo Robot' src={logo} className='logo'/>
       <form className='login-form'>
-        
+      
       </form>
         <div className='buttons-container'>
           <Button class='login' />
