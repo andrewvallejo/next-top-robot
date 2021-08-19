e22b208afaac254f19fbc0428cf25668dbe9d91a
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-19 13:51:46 -0600

    Add entire form for login

--- src/components/LoginPortal.js
+++ src/components/LoginPortal.js
@@ -14,13 +14,49 @@ export const LoginPortal = ({login, error}) => {
     <section className='login-board'>
       <article className='portal'>
       <img alt='A logo of Mondo Robot' src={logo} className='logo'/>
-      <form className='login-form'>
-      
-      </form>
-        <div className='buttons-container'>
-          <Button class='login' />
-          <Button class='register' />
+      <form className='login-form' onSubmit={submitHandler}>
+        {(!!error) ? (<div className='error'>{error}</div>) : ''}
+        <div className='form-inputs'>
+          <label htmlFor='name'>
+            <fieldset className='input-label'>
+              <legend className='input-name'>Full name</legend>
+              <input 
+              type='text' 
+              name='name' 
+              id='name' 
+              value={credentials.name}
+              onChange={e => setCredentials({...credentials, name: e.target.value })} />
+            </fieldset>
+          </label>
         </div>
+        <div className='form-inputs'>
+          <label htmlFor='email'>
+            <fieldset className='input-label'>
+              <legend className='input-name'>Email</legend>
+              <input type='text' email='email' id='email' 
+              value={credentials.email}
+              onChange={e => setCredentials({...credentials, email: e.target.value })} 
+              />
+            </fieldset>
+          </label>
+        </div>
+        <div className='form-inputs'>
+          <label htmlFor='password'>
+            <fieldset className='input-label'>
+              <legend className='input-name'>Password</legend>
+              <input type='password' password='password' id='password'
+              autoComplete='on'
+              value={credentials.password}
+              onChange={e => setCredentials({...credentials, password: e.target.value })} /> 
+            </fieldset>
+          </label>
+        </div>
+        <div className='buttons-container'>
+          <Button type='submit' value='Log In' palette='primary' />
+          <Button value='Register' palette='secondary' />
+        </div>
+
+      </form>
       </article>
     </section>
   )
