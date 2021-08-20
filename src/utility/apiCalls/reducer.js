c4809c150630425ebf3904fc519f411a12416596
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-20 15:16:51 -0600

    Add reducer with login, logout, and localStorage

---
+++ src/utility/apiCalls/reducer.js
@@ -0,0 +1,23 @@
+export const reducer = (state, action) => {
+  const {type, info: {data, name}} = action
+  switch (type) {
+    case "LOGIN":
+      localStorage.setItem("user", JSON.stringify(name));
+      localStorage.setItem("token", JSON.stringify(data));
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: name,
+        token: data
+      };
+    case "LOGOUT":
+      localStorage.clear();
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: ''
+      };
+    default:
+      return state;
+  }
+};
