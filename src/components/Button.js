72b17a42701922476629f264838393cdfa2df331
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-19 19:21:03 -0600

    Refactor button into switch statement

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,41 @@
 import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+
 
 export const Button = ({ value, palette }) => {
   const [active, setActive] = useState(false)
   const [theme, setTheme] = useState('primary')
 
-
-
-
-  return (
-    <button 
-      className={`button 
-        ${palette === theme ? theme : setTheme('secondary')}
-        ${active ? 'active' : ' '} `}
-        onClick={() => setActive(!active)} >
-        {value}
-        {/* {active ? 'Vote Cast' : 'Vote'} */}
-    </button>
-  )
+  switch (value) {
+    case 'Vote':
+      return (
+        <button 
+          className={`button 
+          ${palette === theme ? theme : setTheme('secondary')}
+          ${active ? 'active' : ' '} `}
+          onClick={() => setActive(!active)} >
+          {active ? 'Vote Cast' : 'Vote'}
+         </button>)
+    case 'Register':
+      return (
+        <NavLink
+          className={`button 
+          ${palette === theme ? theme : setTheme('secondary')}
+          ${active ? 'active' : ' '} `}
+          onClick={() => setActive(!active)} 
+          to='/robots' >
+          {value}
+        </NavLink>
+      )
+    default:
+      return (
+        <button 
+          className={`button 
+          ${palette === theme ? theme : setTheme('secondary')}
+          ${active ? 'active' : ' '} `}
+          onClick={() => setActive(!active)} >
+          {value}
+        </button>
+      )
+  }
 }
