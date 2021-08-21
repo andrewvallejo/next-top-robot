75665624252bba6f63f1112e295fc0fea75ae885
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-20 19:27:44 -0600

    Update Loading JSX

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,7 +5,13 @@ import logo from '../assets/mr-logo.png'
 export const Loading = () => {
   return (
     <section className='load-container'> 
+      <h2 className='website-title'>Next Top Robot</h2>
+      <article className='box-covers' />
+        <div className='box one'/>
+        <div className='box two'/>
+      <div/>
       <img alt='A logo of Mondo Robot' src={logo} className='logo'/>
+      <article className='backdrop'/>
     </section>
   )
 }
