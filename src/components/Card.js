1706e2b6bd95d281a2fb9201232ea189b8c11aee
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-21 12:20:29 -0600

    Refactor card to take in API

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Button } from './Button'
-import bender from '../assets/bender.png'
 
+export const Card = ({ robots }) => {
+  
+  return robots.map((robot) => {
+    const {id, url, name} = robot
 
-export const Card = () => {
-  return (
-    <article className='card'>
-      <h3>Bender</h3>
-      <img className='robot-image' alt='Bender' src={bender} />
-      <Button value='Vote' palette='primary' />
-    </article>
-  )
+    return (
+      <article className='card' key={id} id={id}>
+        <h3>{name}</h3>
+        <img className='robot-image' alt={`The robot named ${name}`} src={url} />
+        <Button value='Vote' palette='primary' />
+      </article>
+    )
+  })
 }
