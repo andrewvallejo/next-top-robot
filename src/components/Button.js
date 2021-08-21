ce33987836f3cf2c0e7d198511f4578afcb97add
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-21 14:15:28 -0600

    Refactor to correctly work with toggled states

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,41 +2,40 @@ import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 
-export const Button = ({ value, palette }) => {
-  const [active, setActive] = useState(false)
-  const [theme, setTheme] = useState('primary')
-
+export const Button = ({ value, palette, activate, isVoted }) => {  
   switch (value) {
     case 'Vote':
       return (
         <button 
-          className={`button 
-          ${palette === theme ? theme : setTheme('secondary')}
-          ${active ? 'active' : ' '} `}
-          onClick={() => setActive(!active)} >
-          {active ? 'Vote Cast' : 'Vote'}
+        className={`button ${palette}`} 
+        onClick={() => activate()}>
+          {value}
         </button>
       )
-    case 'Register':
-      return (
-        <NavLink
-          className={`button 
-          ${palette === theme ? theme : setTheme('secondary')}
-          ${active ? 'active' : ' '} `}
-          onClick={() => setActive(!active)} 
-          to='/robots' >
+    case 'Vote Cast':
+       return (
+        <button 
+        className={`button ${palette}`}
+        disabled={isVoted}>
           {value}
-        </NavLink>
-      )
+        </button>
+       )
+    // case 'Register':
+    //   return (
+    //     <NavLink
+    //       className={`button 
+    //       ${palette}
+    //       ${active ? 'active' : ' '} `}
+    //       onClick={() => setActive(!active)} 
+    //       to='/robots' >
+    //       {value}
+    //     </NavLink>
+    //   )
     default:
       return (
-        <button 
-          className={`button 
-          ${palette === theme ? theme : setTheme('secondary')}
-          ${active ? 'active' : ' '} `}
-          onClick={() => setActive(!active)} >
-          {value}
-        </button>
+        <button className={`button ${palette}`}>
+        {value}
+      </button>
       )
   }
 }
