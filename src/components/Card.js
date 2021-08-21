c35dd6a1894ab755e1f3cbb1bbd51e03d5881b84
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-21 14:40:57 -0600

    Refactor card to bubble up a callback

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,23 @@
 import React, { useState } from 'react'
 import { Button } from './Button'
 
-export const Card = ({ robotInfo: {id, url, name} }) => {
+export const Card = ({ robotInfo: {url, name}, vote, hasVoted}) => {
   const [active, setActive]= useState(false)
 
-    const handleVote = () => {
+    const handleButton = () => {
       setActive(true)
+      vote()
     }
 
     return (
-      <article className='card' key={id} id={id}>
+      <article className='card' >
         <h3>{name}</h3>
         <img className='robot-image' alt={`The robot named ${name}`} src={url} />
         <Button 
           palette='primary'
-          activate={handleVote}
-          value={active ? 'Vote Cast' : 'Vote'}
-          isVoted={active}
-          /> 
+          activate={handleButton}
+          value={hasVoted ? 'Vote Cast' : 'Vote'}
+          isVoted={active} /> 
       </article>
     )
 
