25e8b9f691b63e5225d5fd2018922d417b0ee658
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-20 19:33:43 -0600

    Remove unnecessary class

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -7,8 +7,8 @@ export const Loading = () => {
     <section className='load-container'> 
       <h2 className='website-title'>Next Top Robot</h2>
       <article className='box-covers' />
-        <div className='box one'/>
-        <div className='box two'/>
+        <div className='box'/>
+        <div className='box'/>
       <div/>
       <img alt='A logo of Mondo Robot' src={logo} className='logo'/>
       <article className='backdrop'/>
