01bfd47baaefa5041057c0a51c13ebff038855f3
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-20 20:18:03 -0600

    Update class names

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,11 +4,11 @@ import logo from '../assets/mr-logo.png'
 
 export const Loading = () => {
   return (
-    <section className='load-container'> 
+    <section className='load'> 
       <h2 className='website-title'>Next Top Robot</h2>
       <article className='box-covers' />
-        <div className='box'/>
-        <div className='box'/>
+        <div className='box one'/>
+        <div className='box two'/>
       <div/>
       <img alt='A logo of Mondo Robot' src={logo} className='logo'/>
       <article className='backdrop'/>
