6571174a64da00fef996123ff8238019f0710760
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-21 11:56:03 -0600

    Add useEffect and state to draw all the robots

--- src/components/RobotBoard.js
+++ src/components/RobotBoard.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import { retrieveRobots } from '../utility/apiCalls/apiCalls';
+import { AuthContext } from '../utility/apiCalls/AuthContext';
 import { Card } from './Card'
 
 export const RobotBoard = () => {
+  const { state: { token } } = useContext(AuthContext)
+  const [robots, setRobots] = useState([])  
+
+  useEffect(() => {
+    (async () => {
+      await retrieveRobots(token)
+      .then((data) => setRobots(data))
+    })()
+  }, [token])
+
   return (
     <section className='robot-board'>
        <Card />
