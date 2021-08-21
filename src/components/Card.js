78006214e012f9daf620aac0f8963b17d1ca76d1
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-21 14:16:05 -0600

    Refactor Card to work dynamically with buttons

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from './Button'
 
-export const Card = ({ robots }) => {
-  
-  return robots.map((robot) => {
-    const {id, url, name} = robot
+export const Card = ({ robotInfo: {id, url, name} }) => {
+  const [active, setActive]= useState(false)
+
+    const handleVote = () => {
+      setActive(true)
+    }
 
     return (
       <article className='card' key={id} id={id}>
         <h3>{name}</h3>
         <img className='robot-image' alt={`The robot named ${name}`} src={url} />
-        <Button value='Vote' palette='primary' />
+        <Button 
+          palette='primary'
+          activate={handleVote}
+          value={active ? 'Vote Cast' : 'Vote'}
+          isVoted={active}
+          /> 
       </article>
     )
-  })
+
 }
