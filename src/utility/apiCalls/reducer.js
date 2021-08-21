c1e67319141b0caaa0a9ea565078a8644417c901
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-20 22:41:33 -0600

    Remove user from being an item to set

--- src/utility/apiCalls/reducer.js
+++ src/utility/apiCalls/reducer.js
@@ -1,13 +1,11 @@
 export const reducer = (state, action) => {
-  const {type, info: {data, name}} = action
+  const {type, info: { data }} = action
   switch (type) {
     case "LOGIN":
-      localStorage.setItem("user", JSON.stringify(name));
       localStorage.setItem("token", JSON.stringify(data));
       return {
         ...state,
         isAuthenticated: true,
-        user: name,
         token: data
       };
     case "LOGOUT":
