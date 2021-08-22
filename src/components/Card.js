b2d2cd1efdb915b77e19132c331e77102cc27b6a
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-21 18:26:35 -0600

    Refactor button to conditionally render with url

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,38 @@
 import React, { useState } from 'react'
+import { useRouteMatch } from 'react-router-dom'
 import { Button } from './Button'
 
 export const Card = ({ robotInfo: {id, url, name}, vote, hasVoted}) => {
   const [active, setActive]= useState(false)
+  const match = useRouteMatch().path
+  let button
+
+
+  switch (match) {
+      case '/':
+        button = (
+          <Button 
+          palette='primary'
+          activate={() => handleButton(id)}
+          value='Vote'
+          isActive={active}
+          isVoted={hasVoted} />
+        )
+        break;
+      case '/results': 
+        button = (
+          <Button 
+          palette='primary'
+          activate={() => handleButton(id)}
+          value='Vote'
+          isActive={active}
+          isVoted={hasVoted} />
+        )
+      break;
+      default:
+        break;
+    }
+
   
     const handleButton = (id) => {
         setActive(!active)
@@ -12,12 +42,7 @@ export const Card = ({ robotInfo: {id, url, name}, vote, hasVoted}) => {
       <article className='card' >
         <h3>{name}</h3>
         <img className='robot-image' alt={`The robot named ${name}`} src={url} />
-        <Button 
-          palette='primary'
-          activate={() => handleButton(id)}
-          value='Vote'
-          isActive={active}
-          isVoted={hasVoted} /> 
+        {button}
       </article>
     )
 }
