c0964df858fb04f177ffe3708a4a70e3aa1d11f6
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-22 12:05:02 -0600

    Fix card value to say code

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,15 +12,15 @@ export const Card = ({ robotInfo: {id, url, name}, vote, hasVoted, results}) => 
           <Button 
           palette='primary'
           activate={() => handleButton(id)}
-          value='Vote'
+          code='vote'
           isActive={active}
           isVoted={hasVoted} />
         )
       } else if (path === '/results') {
         insert = 
         <>
-        <label htmlFor="results"><h2>{results.length}<span className='out-of'>/55</span></h2></label>
-        <progress className='results-bar' htmlFor="results" value={results} max="55">70 %</progress>
+          <label htmlFor="results"><h2>{results.length}<span className='out-of'>/55</span></h2></label>
+          <progress className='results-bar' htmlFor="results" value={results} max="55">70 %</progress>
         </>
       } 
 
@@ -28,7 +28,7 @@ export const Card = ({ robotInfo: {id, url, name}, vote, hasVoted, results}) => 
     const handleButton = (id) => {
         setActive(!active)
         vote(id)
-  }
+    }
 
 
     return (
