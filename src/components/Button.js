af154fa83fa9a5158ae0109116621cad854c826c
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-22 12:05:28 -0600

    Refactor register button to trigger url change

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,11 @@ import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 
-export const Button = ({ value, palette, activate, isVoted , isActive}) => {  
+export const Button = ({ code, value, palette, activate, isVoted , isActive}) => {  
   const [isTriggered, setIsTriggered] = useState(false)
 
-  switch (value) {
-    case 'Vote':
+  switch (code) {
+    case 'vote':
       return (
         <button 
         className={`button ${palette} ${isTriggered && 'disable'} vote`} 
@@ -15,31 +15,20 @@ export const Button = ({ value, palette, activate, isVoted , isActive}) => {
           {isTriggered ? 'Vote Cast' : 'Vote'}
         </button>
       )
-    case 'Register':
+    case 'register':
       return (
         <NavLink
           className={`button 
           ${palette}`}
           onClick={() => setIsTriggered(!isTriggered)} 
           to={isTriggered ? '/login' : '/registration'}>
-          {isTriggered ? 'Back to Login' : value}
+          {value}
         </NavLink>
       ) 
-      case 'Results':
-        return (
-          <h1> lol</h1>
-          // <NavLink
-          //   className={`button 
-          //   ${palette}
-          //   ${isTriggered ? 'active' : ' '} `}
-          //   onClick={() => setIsTriggered(!isTriggered)} 
-          //   to='/robots' >
-          //   {value}
-          // </NavLink>
-        )
+  
     default:
       return (
-        <button className={`button ${palette}`}>
+      <button className={`button ${palette}`}>
         {value}
       </button>
       )
