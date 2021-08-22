02ed05431c244f78ec33bcbdd19a6f5ccfa55999
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-21 19:25:49 -0600

    Refactor switch to be if else statement

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,14 @@
 import React, { useState } from 'react'
-import { useRouteMatch } from 'react-router-dom'
+import { useLocation, useRouteMatch } from 'react-router-dom'
 import { Button } from './Button'
 
 export const Card = ({ robotInfo: {id, url, name}, vote, hasVoted}) => {
   const [active, setActive]= useState(false)
-  const match = useRouteMatch().path
-  let button
+  const path = useLocation().pathname
+  let insert;
 
-
-  switch (match) {
-      case '/':
-        button = (
+      if (path === '/robots') {
+        insert =  (
           <Button 
           palette='primary'
           activate={() => handleButton(id)}
@@ -18,31 +16,26 @@ export const Card = ({ robotInfo: {id, url, name}, vote, hasVoted}) => {
           isActive={active}
           isVoted={hasVoted} />
         )
-        break;
-      case '/results': 
-        button = (
-          <Button 
-          palette='primary'
-          activate={() => handleButton(id)}
-          value='Vote'
-          isActive={active}
-          isVoted={hasVoted} />
-        )
-      break;
-      default:
-        break;
-    }
+      } else if (path === '/results') {
+        insert = 
+        <>
+        <label for="results"><h2>11<span className='out-of'>/55</span></h2></label>
+        <progress className='results-bar' for="results" value="11" max="55">70 %</progress>
+        </>
+      } 
 
   
     const handleButton = (id) => {
         setActive(!active)
         vote(id)
   }
+
+
     return (
       <article className='card' >
         <h3>{name}</h3>
         <img className='robot-image' alt={`The robot named ${name}`} src={url} />
-        {button}
+        {insert}
       </article>
     )
 }
