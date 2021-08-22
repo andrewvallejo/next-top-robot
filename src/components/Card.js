6111b5da8fb3fde196c145c3fcdd94b9e0b0fbd4
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-22 14:06:08 -0600

    Add functionality to update votes per robot

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { Button } from './Button'
 
-export const Card = ({ robotInfo: {id, url, name}, vote, hasVoted, results}) => {
+export const Card = ({ robotInfo: {id, url, name}, vote, hasVoted, totalVotes}) => {
   const [active, setActive]= useState(false)
+  const [count, setCount] = useState(0)
   const path = useLocation().pathname
   let insert;
+
+  
+  useEffect(() => {
+    const robotTotalVotes = totalVotes.filter((vote) => vote === id)
+    setCount(() => robotTotalVotes.length)
+  }, [totalVotes, id ])
+
+  
+
 
       if (path === '/robots') {
         insert =  (
@@ -17,10 +27,11 @@ export const Card = ({ robotInfo: {id, url, name}, vote, hasVoted, results}) => 
           isVoted={hasVoted} />
         )
       } else if (path === '/results') {
+
         insert = 
         <>
-          <label htmlFor="results"><h2>{results.length}<span className='out-of'>/55</span></h2></label>
-          <progress className='results-bar' htmlFor="results" value={results} max="55">70 %</progress>
+          <label htmlFor="results"><h2 >{count}<span className='out-of'>/{totalVotes.length}</span></h2></label>
+          <progress className='results-bar' htmlFor="results" value={count}  onChange={() => totalVotes} max={totalVotes.length} />
         </>
       } 
 
