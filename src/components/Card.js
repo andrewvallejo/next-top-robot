3abc065f72f9baeff64d37181489b89eb4b62c84
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-22 17:12:11 -0600

    Add logic for button appearences

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,40 +7,50 @@ export const Card = ({ robotInfo: {id, url, name}, vote, hasVoted, totalVotes}) 
   const [count, setCount] = useState(0)
   const path = useLocation().pathname
   let insert;
-
   
   useEffect(() => {
     const robotTotalVotes = totalVotes.filter((vote) => vote === id)
     setCount(() => robotTotalVotes.length)
   }, [totalVotes, id ])
 
-  
+ 
+  const handleButton = (id) => {
+    setActive(!active)
+    vote(id)
+  }
 
 
-      if (path === '/robots') {
-        insert =  (
-          <Button 
-          palette='primary'
-          activate={() => handleButton(id)}
-          code='vote'
-          isActive={active}
-          isVoted={hasVoted} />
-        )
-      } else if (path === '/results') {
-
-        insert = 
-        <>
-          <label htmlFor="results"><h2 >{count}<span className='out-of'>/{totalVotes.length}</span></h2></label>
-          <progress className='results-bar' htmlFor="results" value={count}  onChange={() => totalVotes} max={totalVotes.length} />
-        </>
-      } 
-
-  
-    const handleButton = (id) => {
-        setActive(!active)
-        vote(id)
-    }
-
+  if (path === '/robots') {
+    insert =  (
+      <Button 
+      palette='primary'
+      activate={() => handleButton(id)}
+      code='vote'
+      isActive={active}
+      isVoted={hasVoted} />
+    )
+  } else if (path === '/results') {
+    insert = 
+    <>
+      <label htmlFor="results"><h2 >{count}<span className='out-of'>/{totalVotes.length}</span></h2></label>
+      <progress className='results-bar' htmlFor="results" value={count}  max={totalVotes.length} />
+    </>
+  } else if (path === '/admin') {
+    insert = (
+      <div className='buttons-container'>
+      <Button 
+      palette='primary'
+      activate={() => handleButton(id)}
+      value='Edit'
+      isActive={active}/>
+      <Button 
+      palette='secondary'
+      activate={() => handleButton(id)}
+      value='Delete'
+      isActive={active}/>
+      </div>
+    )
+  }
 
     return (
       <article className='card' >
