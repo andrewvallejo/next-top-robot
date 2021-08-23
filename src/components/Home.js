e0007ed34d71c534c23bd2ae7f15b13328c6d7d7
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-23 16:23:24 -0600

    Add conditonal so admin card works only for admin

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useLocation } from 'react-router-dom'
+import { AuthContext } from '../utility/AuthContext'
 import { AdminCard } from './AdminCard'
 import { BoardHeader } from './BoardHeader'
 import { NavBar } from './NavBar'
 import { RobotCards } from './RobotCards'
 
 export const Home = () => {
+  const { state: { isAdmin } } = useContext(AuthContext)
+  const location = useLocation().pathname
+
   return (
     <>
       <NavBar /> 
       <main>       
         <BoardHeader />
         <section className='robot-board'>
-        <AdminCard />
+        {isAdmin  && location === '/admin' && <AdminCard />}
         <RobotCards />
         </section>
       </main>
