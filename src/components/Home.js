44e520fbc5ae2bc2d5e8fa645ff5e47a84bda291
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-24 15:46:35 -0600

    Refactor burgermenu to be outside the page flow

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,37 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { AuthContext } from '../utility/AuthContext'
 import { BoardHeader } from './BoardHeader'
+import { BurgerMenu } from './BurgerMenu'
 import { Card } from './Card'
 import { NavBar } from './NavBar'
 import { RobotCards } from './RobotCards'
 
 export const Home = () => {
   const { state: { isAdmin } } = useContext(AuthContext)
+  const [isMenuActive, setIsMenuActive] = useState(false)
   const location = useLocation().pathname
+
+  const handleMenu = () => {
+    setIsMenuActive(!isMenuActive)
+  }
 
   return (
     <>
-      <NavBar /> 
-      <main>       
-        <BoardHeader />
-        <section className='robot-board'>
-        {isAdmin  && location === '/admin' && <Card  robotInfo='' totalVotes={[]}/>}
-        <RobotCards />
-        </section>
-      </main>
+      <BurgerMenu toggleMenu={handleMenu}/> 
+      {!isMenuActive && 
+        <>
+          <NavBar /> 
+          <main>       
+            <BoardHeader />
+            <section className='robot-board'>
+              {isAdmin  && location === '/admin' && 
+              <Card  robotInfo='' totalVotes={[]}/>}
+              <RobotCards />
+            </section>
+          </main>
+        </>
+      }
     </>
   )
 }
