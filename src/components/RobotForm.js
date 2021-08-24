bbcccc4a0dcf9021bc172dbc0e6f202447677367
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-23 22:03:30 -0600

    Refactor robot form to mimic the wireframe

--- src/components/RobotForm.js
+++ src/components/RobotForm.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { AuthContext } from '../utility/AuthContext';
+
 import FormData from 'form-data';
 import { addRobot } from '../utility/apiCalls';
 import { Button } from './Button'
@@ -8,47 +10,63 @@ export const RobotForm = () =>{
   const initialState = {
     name: '',
     image: '',
-  }
-
-  const handleInput = (event) => {
-    event.preventDefault();
-    setRobot({...robot, [event.target.name]: event.target.value})
-  }
-
-  const handleSubmit = async(event) => {
-    event.preventDefault();
-    await addRobot(robot)
+    inserted: 'false'
   }
 
   const [robot, setRobot] = useState({ initialState })
+  const { state: { token } } = useContext(AuthContext)
+
+  const handleFile = (event) => {
+    event.preventDefault()
+    setRobot({...robot, [event.target.name]: event.target.value})
+  }
+
+  const handleUpload = async(event) => {
+    event.preventDefault()
+    if(robot.name && robot.image) {
+      setRobot({...robot, inserted: true})
+      const formData = new FormData()
+      formData.append('name', robot.name)
+      formData.append('image', robot.image)
+      await addRobot(formData, token)}
+    else {
+      setRobot({...robot, inserted: false})
+    }
+  }
+
 
   return (
-    <form className='robot-form ' onSubmit={handleSubmit}>
+    <form className='robot-form ' onSubmit={handleUpload}>
+        <h3 className='form-title'>Add Robot</h3>
       <div className='form-inputs'>
-        <h3>Add Robot</h3>
         <label htmlFor='robot-name' className='robot-label'>
           <fieldset className='input-label'>
             <legend className='input-name'>Name</legend>
             <input 
             className='inner-input' 
             type='text' 
-            name='robot-name' 
-            id='robot-name' 
-            onChange={(e) => handleInput(e)} />
+            name='name' 
+            id='name' 
+            onChange={(e) => handleFile(e)} />
           </fieldset>
         </label>
       </div>
+      <div className='file-container'>
+        <label for='file' className='file-label'>Select image to upload</label>
+        <input id='file' type='file' name='file' className='file-input' onChange={(e) => handleFile(e)} />
 
+      </div>
       <div className='buttons-container'>
       <Button 
-      code='create'
+      code='create-valid'
       palette='tertiary'
       value='Clear'
        />
       <Button 
-      disable='disable'
+      code={robot.inserted ? 'create-valid' : 'create-invalid'}
+      type='submit'
       palette='primary'
-      code='create'
+      // disable='disable'
       value='Add Robot' />
       </div>
     </form>
