b1ce45f741c36156ea72656cd59012057668fff8
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-24 15:36:55 -0600

    Refactor to conditional render class based on url

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,25 +1,21 @@
 import React, { useContext } from 'react';
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import { AuthContext } from '../utility/AuthContext';
-import { BurgerMenu } from './BurgerMenu';
 
-export const MobileMenu = () => {
-  const { dispatch, state: authState } = useContext(AuthContext);
-  const { isAdmin, isAuthenticated, token } = authState
-  return (
-     
+export const MobileMenu = ({ logout }) => {
+  const { state: authState } = useContext(AuthContext);
+  const { isAdmin } = authState
+  const location = useLocation().pathname
+
+  return (     
       <nav className='mobile-menu'>
         <div className='links-container'>
-        <NavLink className='mobile-link' to='/robots'>
-        </NavLink>
-          <NavLink className='mobile-link' to='/robots'>Robots</NavLink>
-          <NavLink className='mobile-link' to='/results'>Results</NavLink>
-          {isAdmin && <NavLink className='mobile-link' to='/admin'>Admin</NavLink>}
-          <NavLink className='mobile-link' to= '/'>Log Out</NavLink>
-          </div>
+            <NavLink className={`mobile-link ${location === '/robots' && 'active'}`}  to='/robots'>Robots</NavLink>
+            <NavLink className={`mobile-link ${location === '/result' && 'active'}`}   to='/results'>Results</NavLink>
+            {isAdmin && <NavLink className={`mobile-link ${location === '/admin' && 'active'}`}   to='/admin'>Admin</NavLink>}
+            <NavLink className='mobile-link inactive' to='/' onClick={() => logout()} >Log Out</NavLink>
+        </div>
       </nav>
-    
-
   )
 }
 
