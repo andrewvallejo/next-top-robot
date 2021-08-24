e3add82e864e9e7fbdbf786ab30b54496b4a838a
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-24 15:47:29 -0600

    Add handle methods to make the menu functional

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -1,13 +1,33 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import { expireSession } from '../utility/apiCalls';
+import { AuthContext } from '../utility/AuthContext';
+import { MobileMenu } from './MobileMenu';
 
-export const BurgerMenu = () => {
+export const BurgerMenu = ({ toggleMenu }) => {
   const [focus, setFocus] = useState(false)
+  const { dispatch, state: authState } = useContext(AuthContext);
+  const { isAuthenticated, token } = authState
+
+  const handleToggle = () => {
+    setFocus(!focus)
+    toggleMenu()
+  }
+
+  const handleLogOut = async() => {
+    await expireSession(token)
+    .then(() => !isAuthenticated)
+    .then(() => {dispatch({type: 'LOGOUT', info: {}}) 
+  })
+    .catch((error) => console.error(error))
+  }
 
   return (
-    <div className='burger-menu' onClick={() => setFocus(!focus)}>
-      <div className={`line one ${focus ? 'focus' : ' '}`} />
-      <div className={`line two ${focus ? 'focus' : ' '}`} />
-      <div className={`line three ${focus ? 'focus' : ' '}`}/>
-    </div>
+    <div className='burger-menu' onClick={handleToggle}>
+          <div className={`line one ${focus ? 'focus' : ' '}`} />
+          <div className={`line two ${focus ? 'focus' : ' '}`} />
+          <div className={`line three ${focus ? 'focus' : ' '}`}/>  
+          {focus && <MobileMenu logout={handleLogOut}/>}  
+        </div>
   )
 }
+
