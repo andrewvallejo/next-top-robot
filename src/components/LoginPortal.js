2208deacf913436fdef8f608ae86e3f43fa2ec3f
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-24 18:22:32 -0600

    Fix indentation

--- src/components/LoginPortal.js
+++ src/components/LoginPortal.js
@@ -45,78 +45,78 @@ export const LoginPortal = () => {
     }) 
   }
 
-const handleRegistration = async(event) => {
-  event.preventDefault();
-  await registerUser(credentials)
-  .then(() => {
-    setCredentials({...credentials, registered: true, error: 'Registered!'})
-    history.push("/login");
-    })
-  .catch(() => {
-    event.preventDefault()
-    setCredentials({...credentials, error: 'Fill out all required information'})
-  }) 
-}
+  const handleRegistration = async(event) => {
+    event.preventDefault();
+    await registerUser(credentials)
+    .then(() => {
+      setCredentials({...credentials, registered: true, error: 'Registered!'})
+      history.push("/login");
+      })
+    .catch(() => {
+      event.preventDefault()
+      setCredentials({...credentials, error: 'Fill out all required information'})
+    }) 
+  }
 
   return (
     <>
-    {(credentials.authenticated) ? (<Loading className='login-load'/>) :
-    <section className='login-board'>
-      <article className={`portal`}>
-      <img alt='A logo of Mondo Robot' src={logo} className='logo'/>
-      <form className='login-form' onSubmit={handleSubmit}>
-        {(!!credentials.error) && (<div className='error'>{credentials.error}</div>)}
-        {isRegistration && !credentials.registered &&
+      {(credentials.authenticated) ? (<Loading className='login-load'/>) :
+      <section className='login-board'>
+        <article className={`portal`}>
+        <img alt='A logo of Mondo Robot' src={logo} className='logo'/>
+        <form className='login-form' onSubmit={handleSubmit}>
+          {(!!credentials.error) && (<div className='error'>{credentials.error}</div>)}
+          {isRegistration && !credentials.registered &&
+            <div className='form-inputs'>
+            <label htmlFor='name'>
+              <fieldset className='input-label'>
+                <legend className='input-name'>Full name</legend>
+                <input 
+                className='inner-input' 
+                type='text' 
+                name='name' 
+                id='name' 
+                value={credentials.name || ''}
+                onChange={(e) => handleInput(e)} />
+              </fieldset>
+            </label>
+          </div>}
           <div className='form-inputs'>
-          <label htmlFor='name'>
-            <fieldset className='input-label'>
-              <legend className='input-name'>Full name</legend>
-              <input 
-              className='inner-input' 
-              type='text' 
-              name='name' 
-              id='name' 
-              value={credentials.name || ''}
-              onChange={(e) => handleInput(e)} />
-            </fieldset>
-          </label>
-        </div>}
-        <div className='form-inputs'>
-          <label htmlFor='email'>
-            <fieldset className='input-label'>
-              <legend className='input-name'>Email</legend>
-              <input
-              className='inner-input' 
-              type='text' name='email' id='email' 
-              value={credentials.email || ''}
-              onChange={(e) => handleInput(e)} 
-              />
-            </fieldset>
-          </label>
-        </div>
-        <div className='form-inputs'>
-          <label htmlFor='password'>
-            <fieldset className='input-label'>
-              <legend className='input-name'>Password</legend>
-              <input type='password' name='password' id='password'
-              className='inner-input' 
-              autoComplete='on'
-              value={credentials.password || ''}
-              onChange={(e) => handleInput(e)} /> 
-            </fieldset>
-          </label>
-        </div>
-        <div className='buttons-container'>
-          <Button disable={credentials.authenticated} 
-          type='submit' 
-          value={isRegistration && !credentials.registered ? 'Register' : 'Login'} 
-          palette='primary' />
-            <Button code='register' value={isRegistration ? 'Back to Login' : 'Register'} palette='secondary'/>
-        </div>
-      </form>
-      </article>
-    </section>
-    }
+            <label htmlFor='email'>
+              <fieldset className='input-label'>
+                <legend className='input-name'>Email</legend>
+                <input
+                className='inner-input' 
+                type='text' name='email' id='email' 
+                value={credentials.email || ''}
+                onChange={(e) => handleInput(e)} 
+                />
+              </fieldset>
+            </label>
+          </div>
+          <div className='form-inputs'>
+            <label htmlFor='password'>
+              <fieldset className='input-label'>
+                <legend className='input-name'>Password</legend>
+                <input type='password' name='password' id='password'
+                className='inner-input' 
+                autoComplete='on'
+                value={credentials.password || ''}
+                onChange={(e) => handleInput(e)} /> 
+              </fieldset>
+            </label>
+          </div>
+          <div className='buttons-container'>
+            <Button disable={credentials.authenticated} 
+            type='submit' 
+            value={isRegistration && !credentials.registered ? 'Register' : 'Login'} 
+            palette='primary' />
+              <Button code='register' value={isRegistration ? 'Back to Login' : 'Register'} palette='secondary'/>
+          </div>
+        </form>
+        </article>
+      </section>
+      }
     </>
   )
 }
